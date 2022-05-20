045e35962330e77e319dbe4860339865ad98426c
REFSZIN <refwire@gmail.com>
2022-05-20 17:03:16 -0300

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,24 +1,33 @@
 import "./style.js"
-import tumb1 from "../../assets/images/tumb1.png"
-import tumb2 from "../../assets/images/tumb2.png"
 import { Link } from "react-router-dom";
-import {Maiin ,Mainn,H2, ListTumbs, BackTmbMain, Tumb } from "./style.js"
-
+import {Maiin ,Mainn, H2, ListTumbs, BackTmbMain, Tumb } from "./style.js"
+import { useEffect , useState } from "react";
+import axios from "axios";
 export default function Main (){
+    const [filme ,setfilme] =useState([]);
+
+    useEffect(() => {
+        const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+        requisicao.then(resposta => {
+            resposta.data(filme =>{
+                setfilme(resposta.data)
+            })
+        });
+    },[]);
+    
+    const ListTumb = poster.map(poster =>{
+        <BackTmbMain>
+            <Link to={'/sessoes/:${poster.idFilme}'}>
+                <Tumb src={poster.posterURL} alt={poster.idFilme}/>
+            </Link>
+        </BackTmbMain>
+    })
     return(
         <Maiin>
             <Mainn>
                 <H2>Selecione o filme</H2>
                 <ListTumbs>
-                    <BackTmbMain>
-                        <Link to='/sessoes'><Tumb src={tumb1} alt="Thumbnail"/></Link>
-                    </BackTmbMain>
-                    <BackTmbMain>
-                        <Link to='/sessoes'><Tumb src={tumb2} alt="Thumbnail"/></Link>
-                    </BackTmbMain>
-                    <BackTmbMain>
-                        <Link to='/sessoes'><Tumb src={tumb1} alt="Thumbnail"/></Link>
-                    </BackTmbMain>
+                    <ListTumb />
                 </ListTumbs>
             </Mainn>
         </Maiin>
